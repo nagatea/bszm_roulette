aeadc62b6998d7639469d67029dea7ee3a50815a
nagatech <cheesecakebot@yahoo.co.jp>
2019-02-09 23:15:18 +0900

    npm install vue-localstorage

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueMaterial from 'vue-material'
+import VueLocalStorage from 'vue-localstorage'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 
 Vue.use(VueMaterial)
+Vue.use(VueLocalStorage)
 
 Vue.config.productionTip = false
 
