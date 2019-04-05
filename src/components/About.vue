7e69bbd336461184ed140cdd2251c4de4a8ff7c6
nagatech <cheesecakebot@yahoo.co.jp>
2019-04-05 17:43:39 +0900

    modify about

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,7 +4,9 @@
       <md-dialog-title>bszm-rouletteについて</md-dialog-title>
         <md-dialog-content>
           <p>情報理工学院バスゼミのビンゴ用ルーレットアプリです。</p>
-          <p>時間に余裕があったら加筆します。</p>
+          <p>CSVを読みこんで好きなワードでルーレットを回すことができます。</p>
+          <p>製作者: <a href="https://twitter.com/_nagatech" target="_blank">@_nagatech</a></p>
+          <p><a href="https://github.com/nagatea/bszm_roulette" target="_blank">Please your contribution!</a></p>
         </md-dialog-content>
       <md-dialog-actions>
         <md-button class="md-primary" @click="changeShow()">Close</md-button>
